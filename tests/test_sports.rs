82ba8acb077bc321f0b28754cb9d998d368cc671
Alexander Eichhorn <a.eichhorn@losjet.com>
2025-01-07 22:08:21 +0100

    added more tests

---
+++ tests/test_sports.rs
@@ -0,0 +1,44 @@
+use torrent_title_parser::{parse_title, ParsedTitle};
+
+#[test]
+fn test_random_sports_parse() {
+    let test_cases = vec![
+        (
+            "UFC.239.PPV.Jones.Vs.Santos.HDTV.x264-PUNCH[TGx]",
+            ParsedTitle {
+                title: "UFC 239 Jones Vs Santos".to_string(),
+                quality: Some("HDTV".to_string()),
+                codec: Some("avc".to_string()),
+                group: Some("PUNCH".to_string()),
+                ppv: true,
+                ..Default::default()
+            },
+        ),
+        (
+            "UFC.Fight.Night.158.Cowboy.vs.Gaethje.WEB.x264-PUNCH[TGx]",
+            ParsedTitle {
+                title: "UFC Fight Night 158 Cowboy vs Gaethje".to_string(),
+                quality: Some("WEB".to_string()),
+                codec: Some("avc".to_string()),
+                group: Some("PUNCH".to_string()),
+                ppv: true,
+                ..Default::default()
+            },
+        ),
+        (
+            "UFC 226 PPV Miocic vs Cormier HDTV x264-Ebi [TJET]",
+            ParsedTitle {
+                title: "UFC 226 Miocic vs Cormier".to_string(),
+                quality: Some("HDTV".to_string()),
+                codec: Some("avc".to_string()),
+                ppv: true,
+                ..Default::default()
+            },
+        ),
+    ];
+
+    for (release_name, expected) in test_cases {
+        let result = parse_title(release_name).unwrap();
+        assert_eq!(result, expected, "Failed for {}", release_name);
+    }
+}
